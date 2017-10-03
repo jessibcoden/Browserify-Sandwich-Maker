5026f99f1186e5d2e5d7e342c4c73e1f5e114328
Jess Brawner <jessnbillbrawner@gmail.com>
2017-10-03 17:11:27 -0500

    added build sandwich functionality

--- javascripts/events.js
+++ javascripts/events.js
@@ -1,1 +1,42 @@
 "use strict";
+
+const toppingsData = require('./data');
+const printToDom = require('./dom');
+
+let builder = document.getElementById("builder").addEventListener('click', (event) => {
+	if(event.target.classList.contains('checkbox')){
+		buildSandwich(event);
+	}
+});
+
+let mySandwich = [];
+
+function buildSandwich(event){
+	console.log("event", event);
+	if(event.target.checked === true){
+		mySandwich.push({topping: event.target.name, price: event.target.value});
+	}
+	else if(event.target.checked === false){
+		for(let j = 0; j < mySandwich.length; j++){
+			console.log("mysand j", mySandwich[j]);
+			console.log("target name", event.target.name);
+			if(mySandwich[j].topping === event.target.name){
+				mySandwich.splice(j, 1);			
+			}		
+
+		}
+	}
+	let sandString = "";
+	let totalPrice = 0;
+	mySandwich.forEach((sandwich) => {
+		sandString += `<p>${sandwich.topping}</p>`;
+		totalPrice += parseFloat(sandwich.price); 
+	});
+		sandString += `<p>Total Price: $${totalPrice.toFixed(2)}</p>`;
+
+	writeToDom(sandString);	
+}
+
+let writeToDom = (strang) => {
+	document.getElementById("selected-ingredients").innerHTML = strang;
+};		
